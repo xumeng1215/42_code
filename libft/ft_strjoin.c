b209abbd8e5879591e64d9eb83ee69a2d0addaee
charlie at home <charlie_xumeng@hotmail.com>
2024-03-18 22:25:17 +1030

    save

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -18,21 +18,19 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str_joined;
 	size_t	s1_len;
 	size_t	s2_len;
 	char	*p;
 
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	str_joined = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
-	if (str_joined == NULL)
+	p = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (p == NULL)
 		return (NULL);
-	p = str_joined;
 	while (*s1 != '\0')
 		*p++ = *s1++;
 	while (*s2 != '\0')
 		*p++ = *s2++;
 	*p = '\0';
-	return (str_joined);
+	return (p);
 }
